af84ab090f8a8d8bea38b2d598fc207ab61ae324
Cristhian Camargo <cristhian.camargo@usantoto.edu.co>
2022-11-30 17:29:16 -0500

    Modificación de datos de conexión - Base de datos desplegada

--- database/connection.ts
+++ database/connection.ts
@@ -1,7 +1,15 @@
+/*import { Sequelize } from 'sequelize';
+
+const dataBase = new Sequelize('postgresql://postgres:QqHuOpxQrHsqMNz3FtpG@containers-us-west-100.railway.app:6337/railway');
+
+export default dataBase;
+*/
+
 import { Sequelize } from 'sequelize';
 
-const dataBase = new Sequelize('bd_sgsst', 'postgres', '123456', {
-  host: 'localhost',
+const dataBase = new Sequelize('railway', 'postgres', 'QqHuOpxQrHsqMNz3FtpG', {
+  host: 'containers-us-west-100.railway.app',
+  port: 6337,
   dialect: 'postgres',
   define: { 
     freezeTableName: true }
--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,8 +1,15 @@
 "use strict";
+/*import { Sequelize } from 'sequelize';
+
+const dataBase = new Sequelize('postgresql://postgres:QqHuOpxQrHsqMNz3FtpG@containers-us-west-100.railway.app:6337/railway');
+
+export default dataBase;
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const dataBase = new sequelize_1.Sequelize('bd_sgsst', 'postgres', '123456', {
-    host: 'localhost',
+const dataBase = new sequelize_1.Sequelize('railway', 'postgres', 'QqHuOpxQrHsqMNz3FtpG', {
+    host: 'containers-us-west-100.railway.app',
+    port: 6337,
     dialect: 'postgres',
     define: {
         freezeTableName: true
